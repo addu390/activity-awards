22aa7cfe04af43da7e14a18f5e42c324878a42af
Adesh Nalpet <390.adesh@gmail.com>
2021-01-24 01:50:09 +0530

    Activity Award Badges - IOS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HealthKit } from '@ionic-native/health-kit/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    HealthKit,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { HealthKit, HealthKitOptions } from '@ionic-native/health-kit/ngx'
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  stepCount = 'No Data'
 
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private healthKit: HealthKit) {
+
+      this.platform.ready().then(() => {
+        this.healthKit.available().then(available => {
+          if (available) {
+            var options: HealthKitOptions = {
+              readTypes: ['HKQuantityTypeIdentifierStepCount'],
+              writeTypes: []
+            }
+            this.healthKit.requestAuthorization(options).then(allow => {
+              this.loadHealthData();
+            });
+          }
+        })
+      })
+
+  }
+
+  loadHealthData() {
+    var stepOptions = {
+      startDate: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
+      endDate: new Date(),
+      unit: 'count',
+      sampleType: 'HKQuantityTypeIdentifierStepCount' 
+    }
+    this.healthKit.querySampleType(stepOptions).then(data => {
+      this.stepCount = data;
+      console.log("The Step count is: ", data)
+    }, error => {
+      console.log('Error getting Step Count: ', error);
+    });
+  }
 
 }
